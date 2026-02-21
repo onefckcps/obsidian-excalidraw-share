ace4625204bb9a31b4d4ac1841e480523ed7a28c
Simon leyk <simon@leyk.me>
2026-02-21 02:31:52 +0100

    fixed default.nix unused stuff

--- nixos/default.nix
+++ nixos/default.nix
@@ -5,24 +5,18 @@
   pkg-config,
   openssl,
   zstd,
-  nodejs_20,
-  npm,
   ...
 }:
 
 let
-  # Source aus dem gesamten Projekt
   src = lib.cleanSource ../..;
 in
 
 rustPlatform.buildRustPackage rec {
   pname = "excalidraw-share";
   version = "0.1.0";
-
-  # Wir nehmen das gesamte Projekt, da wir später im Modul das Frontend dazubauen
   inherit src;
 
-  # Kein Cargo.lock auf Repo-Ebene, daher deaktivieren wir diese Prüfung
   cargoLock.lockFile = null;
 
   nativeBuildInputs = [ pkg-config ];
@@ -31,8 +25,6 @@ rustPlatform.buildRustPackage rec {
     zstd
   ];
 
-  # Frontend Build (wird nur für das NixOS-Paket genutzt)
-  # Bei manuellem Build nutzt man npm build im frontend/ Verzeichnis
   postBuild = ''
     echo "Excalidraw Share built successfully"
   '';
