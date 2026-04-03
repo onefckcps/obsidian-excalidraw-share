7f1118b98a34c81bad6188d96f3b3c3f7ecb1d71
Simon leyk <simon@leyk.me>
2026-04-03 03:08:34 +0200

    fix: update npmDepsHash for excalishare-frontend package

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
             pname = "excalishare-frontend";
             version = "1.0.1";
             src = ./frontend;
-            npmDepsHash = "sha256-WM+YCHaVMjuCZVSIrhoPdRh0xGb8PAR1ZBlMkcaiy00=";
+            npmDepsHash = "sha256-Qr9bF3EfoMXaAn55KI0hpIwhq4rVlzeoykVtTobCo6U=";
             # The Excalidraw package needs legacy-peer-deps
             npmFlags = [ "--legacy-peer-deps" ];
             installPhase = ''
